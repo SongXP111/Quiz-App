41df6ac43a5e3c29418a71d9505958ccff7eeea1
xipeng <sxp0728@126.com>
2024-07-07 18:31:41 -0400

    create question service interface

---
+++ src/main/java/com/xipeng/quizonline/service/IQuestionService.java
@@ -0,0 +1,23 @@
+package com.xipeng.quizonline.service;
+
+import com.xipeng.quizonline.model.Question;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IQuestionService {
+
+    Question createQuestion(Question question);
+
+    List<Question> getAllQuestions();
+
+    Optional<Question> getQuestionById(Long id);
+
+    List<String> getAllSubjects();
+
+    Question updateQuestion(Long id, Question question);
+
+    void deleteQuestion(Long id);
+
+    List<Question> getQuestionsForUser(Integer numOfQuestions, String subject);
+}
