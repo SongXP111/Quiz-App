7c475d59bb0737f4c6744c0481c933ac7e5bd240
xipeng <sxp0728@126.com>
2024-07-08 20:14:10 -0400

    implements question service and creates question repo interface

--- src/main/java/com/xipeng/quizonline/service/IQuestionService.java
+++ src/main/java/com/xipeng/quizonline/service/IQuestionService.java
@@ -1,6 +1,7 @@
 package com.xipeng.quizonline.service;
 
 import com.xipeng.quizonline.model.Question;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +16,7 @@ public interface IQuestionService {
 
     List<String> getAllSubjects();
 
-    Question updateQuestion(Long id, Question question);
+    Question updateQuestion(Long id, Question question) throws ChangeSetPersister.NotFoundException;
 
     void deleteQuestion(Long id);
 
